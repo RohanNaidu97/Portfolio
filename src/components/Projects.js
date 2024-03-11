ef9d2a9e44d87899313cb2a964623fba42399ab5
RohanNaidu97 <46955750+RohanNaidu97@users.noreply.github.com>
2024-03-11 11:20:38 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,20 +3,20 @@ import "./Projects.css";
 function Projects(){
     return(
         <>
-            <div className="container">
-                <h4>Projects to showcase Javascript skills</h4>
+            <div className="project">
+                <h4 className="title">Projects to showcase Javascript skills</h4>
                 <p>
                     I have developed and am hosting a few of my projects that I used practice my Javascript skills while learning.
                     Following are the active urls:
                 </p>
-                <ul>
+                <ul className="link">
                     <li> <a href="https://rohannaidu97.github.io/Dice-Game/"> Dice-Game </a> </li>
                     <li> <a href="https://rohannaidu97.github.io/Drum-Kit/"> Drum-Kit </a> </li>
                     <li>  <a href="https://rohannaidu97.github.io/Simon-Game/"> Simon-Game </a> </li>
                 </ul>
             </div>
             
-            <div className="container">
+            <div className="project">
                 <h4>RiseUp (Django using Python)</h4>
                 <p>
                     Developed a web application that helps you to set a daily schedule by creating a to-do list, to make a list of
